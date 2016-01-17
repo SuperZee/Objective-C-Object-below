60ee7edc6b4103f9e1eedc9cb5bef453dc1125b8
Zongminglee <zongmingli@outlook.com>
2016-01-17 13:25:24 +0800

    init git repository

---
+++ Objective-C-Object-below/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C-Object-below
+//
+//  Created by SpiColorPendra on 16/1/17.
+//  Copyright © 2016年 SpiColorPendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
